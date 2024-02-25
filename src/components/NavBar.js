ac5789964182da33e77f06abe298be34e3f4f4e5
Drew <dch528@gmail.com>
2024-02-25 17:58:20 -0500

    Drew H Lists and Keys Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // Call the .map method on the "links" array, iterating over it and applying a function, then returns a new array. the link fucntion turns each link string into a react "<a>" or "anchor" element
+  const navLinks = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+
+  return <nav>{navLinks}</nav>;
 }
 
 export default NavBar;
